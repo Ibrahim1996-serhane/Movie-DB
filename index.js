e153de5efe27482e4408e2213c3889b4afda12c5
ibrahim1996-serhane <ibrahim.serhane1996@gmail.com>
2021-06-15 22:02:34 +0300

    step 3 - done

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const express = require('express')
-const application = express()
-const port = 3001
+const express = require('express');
+const application = express();
+const port = 3001;
+const d = new Date();
+const t = "Time is " +d.getHours() + ":" + d.getMinutes();
+
 application.get('/', (req, resend) => {
     resend.send('Ok')
-})
+});
 application.listen(port, () => {
     console.log("application at http://localhost:3001")
-})
+});
+
+application.get("/test", (req, resend) => {
+    resend.status(200).send({ status: 200, message: "ok" });
+  });
+application.get("/time", (req, resend) => {
+    resend.status(200).send({ status: 200, message: t });
+  });
